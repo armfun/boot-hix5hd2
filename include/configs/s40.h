1682bda846013df8e671fbdda6241b77b7b2a0e2
Jiancheng Xue <xuejiancheng@huawei.com>
2014-06-04 17:10:15 +0800

    board: s40: add ft_board_setup     ft_board_setup is used to setup private dtb node passing info from uboot to kernel Signed-off-by: Jiancheng Xue <xuejiancheng@huawei.com>

--- include/configs/s40.h
+++ include/configs/s40.h
@@ -125,6 +125,7 @@
 
 /* pass open firmware flat tree */
 #define CONFIG_OF_LIBFDT	1
+#define CONFIG_OF_BOARD_SETUP	1
  
 /*-----------------------------------------------------------------------
  * for  commond configure
